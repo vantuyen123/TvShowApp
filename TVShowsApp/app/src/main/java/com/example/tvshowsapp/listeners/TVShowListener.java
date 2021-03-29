76b86c90e51d296c1e73e551f30fefad927d19b5
vantuyen123 <68177953+vantuyen123@users.noreply.github.com>
2021-03-29 13:09:01 +0700

    refresh

--- TVShowsApp/app/src/main/java/com/example/tvshowsapp/listeners/TVShowListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.tvshowsapp.listeners;
-
-import com.example.tvshowsapp.models.TVShow;
-
-public interface TVShowListener {
-    void onTVShowClicked(TVShow tvShow);
-}
--- TVShowsApp/app/src/main/java/com/example/tvshowsapp/utillities/TempDataHolder.java
+++
@@ -1,6 +0,0 @@
-package com.example.tvshowsapp.utillities;
-
-public class TempDataHolder {
-
-    public static  boolean IS_WATCHLIST_UPDATE = false;
-}
